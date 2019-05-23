1653902457e97720da01de831c768b363281ac95
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-23 11:42:51 +0200

    Added a loop to create multiple dogs and added more details to the dog

--- petstore.js
+++ petstore.js
@@ -1,10 +1,20 @@
+var allanimals=[];
 class animal{
-    constructor(){
+    constructor(type){
+        this.type=type; 
         this.color="various"; 
         this.price="various";
-        this.type="pet";      
+     
     }
 }
+for (i=1; i<=3; i++){
+    let dog = new animal("dog");
+    allanimals.push(dog)
+        dog.price="500";
+        dog.coat="fur";
+        dog.legs="4";
+        dog.teeth="sharp fangs";
+        dog.id="dog"+i;
+}
 
-let dog = new animal("shepherd");
-console.log(dog.type);
+console.log(allanimals);
