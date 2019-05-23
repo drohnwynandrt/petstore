21e57feb94e161ed7397eebde539c3e83905f095
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-23 11:55:41 +0200

    Added multiple of a new animal (Bunny) with a loop

--- petstore.js
+++ petstore.js
@@ -8,7 +8,7 @@ class animal{
     }
 }
 for (i=1; i<=3; i++){
-    let dog = new animal("dog");
+    let dog = new animal("Dog");
     allanimals.push(dog)
         dog.price="500";
         dog.coat="fur";
@@ -17,4 +17,13 @@ for (i=1; i<=3; i++){
         dog.id="dog"+i;
 }
 
+for (i=1; i<=4; i++){
+    let bunny = new animal("Bunny");
+    allanimals.push(bunny)
+        bunny.price="200";
+        bunny.coat="fur";
+        bunny.legs="4";
+        bunny.teeth="sharp and long";
+        bunny.id="bunny"+i;
+}
 console.log(allanimals);
